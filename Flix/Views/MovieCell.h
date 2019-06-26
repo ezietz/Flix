460a6ed3b822952afa3f4691d8b62a4ac8f51ab4
Elizabeth Zietz <ezietz@fb.com>
2019-06-26 15:29:49 -0700

    App that fulfills the first three required features

---
+++ Flix/Views/MovieCell.h
@@ -0,0 +1,21 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by ezietz on 6/26/19.
+//  Copyright © 2019 ezietz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
