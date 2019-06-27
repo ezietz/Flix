a02e4881e12868e974b9cbdf9a6c6f3b78f88437
Elizabeth Zietz <ezietz@fb.com>
2019-06-27 16:14:08 -0700

    Implemented Collection View and refresh for the Collection View

---
+++ Flix/Views/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  Flix
+//
+//  Created by ezietz on 6/27/19.
+//  Copyright © 2019 ezietz. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
